3abfcc62740fbe03e50c0407ec50371871abfa3a
Annelis da Silva Garcia <134562693+AnnelisDaSilvaGarcia@users.noreply.github.com>
2023-06-26 16:42:49 -0300

    Add files via upload

---
+++ javaScript - Objetos/Aula 05/filtrar.js
@@ -0,0 +1,9 @@
+const clientes = require("./clientes.json");
+
+function filtrarApartamentoSemComplemento(clientes){
+    return clientes.filter((clientes)=>{ return (clientes.endereco.apartamento && !clientes.endereco.hasOwnProperty("complemento"))})
+}
+
+const filtrados = filtrarApartamentoSemComplemento(clientes);
+
+console.log(filtrados);
